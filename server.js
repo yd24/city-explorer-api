d352681ad5a69c81be62f30f83fd2aced03906a0
Daniel Yoon <64057788+yd24@users.noreply.github.com>
2023-03-13 00:09:49 -0700

    Implement cache and movies API request

--- server.js
+++ server.js
@@ -5,21 +5,34 @@ const express = require('express');
 const cors = require('cors');
 
 const weather = require('./modules/weather.js');
+const movies = require('./modules/movies.js');
 const app = express();
 app.use(cors());
 
 app.get('/weather', weatherHandler);
+app.get('/movies', movieHandler);
 
 function weatherHandler(request, response) {
     const { latitude, longitude } = request.query;
-    console.log(request.query);
+
     weather.getWeather(latitude, longitude)
         .then(summaries => {
             response.send(summaries);
         })
         .catch((error) => {
             console.error(error);
-            response.status(200).send('Sorry. Something went wrong!')
+            response.status(500).send('Sorry. Something went wrong!')
+        });
+}
+
+function movieHandler(request, response, next) {
+    movies.getMovies(request, next)
+        .then(movies => {
+            response.send(movies);
+        })
+        .catch((error) => {
+            console.error(error);
+            response.status(500).send('Sorry. Something went wrong!')
         });
 }
 
